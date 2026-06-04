ac481319e9a4cf067f6e9ee8da82d7c76a82a283
khuship-byte <pk02121525@gmail.com>
2026-06-04 15:06:28 +1000

    Update file paths in type definitions for authentication and dashboard components
    - Changed file paths in the type definitions for the login, dashboard, and layout components to reflect the new directory structure.

--- client/.next/types/app/(dashboard)/dashboard/page.ts
+++ client/.next/types/app/(dashboard)/dashboard/page.ts
@@ -1,4 +1,4 @@
-// File: C:\Users\pk021\OneDrive\Desktop\SmartGroup\SmartGroup\client\app\(dashboard)\dashboard\page.jsx
+// File: c:\Users\jasha\OneDrive\Documents\SmartGroup\SmartGroup\client\app\(dashboard)\dashboard\page.jsx
 import * as entry from '../../../../../app/(dashboard)/dashboard/page.js'
 import type { ResolvingMetadata, ResolvingViewport } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
